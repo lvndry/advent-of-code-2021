caece54623ad0ccdb07a7d00759d9431eaa56f05
lvndry <lvndry@protonmail.com>
2021-12-01 23:01:08 +0100

    use iterators only

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,26 +4,27 @@ use std::path::Path;
 fn main() {
     let input = Path::new("./input.txt");
     let contents = fs::read_to_string(input).expect("Something went wrong reading the file");
-    let depths: Vec<i32> = contents.split('\n').map(|n| n.parse().unwrap()).collect();
+    let depths: Vec<u32> = contents.split('\n').map(|n| n.parse().unwrap()).collect();
     // part 1
-    let increased_len = depths
-        .windows(2)
-        .fold(0, |acc, val| if val[1] > val[0] { acc + 1 } else { acc });
+    let increased_depth_count =
+        depths
+            .windows(2)
+            .fold(0, |acc, val| if val[1] > val[0] { acc + 1 } else { acc });
 
-    println!("{}", increased_len);
+    println!("{}", increased_depth_count);
 
     // part 2
-    let mut prev_sum = depths[0] + depths[1] + depths[2];
-    let mut moving_count = 0;
-
-    for i in 1..depths.len() - 2 {
-        let current_sum = depths[i] + depths[i + 1] + depths[i + 2];
+    let mut iter_window = depths.windows(3);
+    let mut prev_sum: u32 = iter_window.next().unwrap().iter().sum();
+    let mut increasead_sum_count = 0;
+    for (_, val) in iter_window.enumerate() {
+        let current_sum: u32 = val.iter().sum();
         if current_sum > prev_sum {
-            moving_count += 1
+            increasead_sum_count += 1;
         }
 
         prev_sum = current_sum
     }
 
-    println!("{}", moving_count);
+    println!("{}", increasead_sum_count);
 }
