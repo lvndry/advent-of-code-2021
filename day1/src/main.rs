5c33b90b7296adcd04c74b8b41ecc9173280c251
lvndry <lvndry@protonmail.com>
2021-12-03 13:13:36 +0100

    day 1 in 1 line

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,13 +6,20 @@ fn main() {
     let contents = fs::read_to_string(input).expect("Something went wrong reading the file");
     let depths: Vec<u32> = contents.split('\n').map(|n| n.parse().unwrap()).collect();
     // part 1
+    let part_1 = depths.windows(2).filter(|w| w[1] > w[0]).count();
+    println!("{}", part_1);
+
+    let part_2 = depths.windows(4).filter(|w| w[3] > w[0]).count();
+    println!("{}", part_2);
+
+    /*
+    // part 1
     let increased_depth_count =
         depths
             .windows(2)
             .fold(0, |acc, val| if val[1] > val[0] { acc + 1 } else { acc });
 
     println!("{}", increased_depth_count);
-
     // part 2
     let mut iter_window = depths.windows(3);
     let mut prev_sum: u32 = iter_window.next().unwrap().iter().sum();
@@ -27,4 +34,5 @@ fn main() {
     }
 
     println!("{}", increasead_sum_count);
+    */
 }
